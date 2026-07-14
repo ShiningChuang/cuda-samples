53dd06f10ecadf11952682dae6289ad71fe36653
lxzhong <49541288+ShiningChuang@users.noreply.github.com>
2026-07-14 14:16:23 -0700

    Add CUDA Tensor Core GEMM study notes

--- cpp/3_CUDA_Features/cudaTensorCoreGemm/cudaTensorCoreGemm.cu
+++ cpp/3_CUDA_Features/cudaTensorCoreGemm/cudaTensorCoreGemm.cu
@@ -135,13 +135,13 @@
 #define CHUNK_COPY_LINE_LANES     (WARP_SIZE / CHUNK_COPY_LINES_PER_WARP)
 
 #define BLOCK_ROW_WARPS 2
-#define BLOCK_COL_WARPS 4
+#define BLOCK_COL_WARPS 4   // 一个 block 在这一维有 4 个 warp
 
 #define WARP_ROW_TILES 4
-#define WARP_COL_TILES 2
+#define WARP_COL_TILES 2    // 每个 warp 在“列/纵向分组”这一维负责 2 个 16x16 tile
 
 #define BLOCK_ROW_TILES (WARP_ROW_TILES * BLOCK_ROW_WARPS)
-#define BLOCK_COL_TILES (WARP_COL_TILES * BLOCK_COL_WARPS)
+#define BLOCK_COL_TILES (WARP_COL_TILES * BLOCK_COL_WARPS) // 整个 block 在这一维共有 2 * 4 = 8 个 16x16 tile，对应实际尺寸：8 * 16 = 128
 
 #define GLOBAL_MEM_STRIDE N_GLOBAL
 
@@ -160,6 +160,7 @@
 // conflicts.
 // The number of 16 two-byte "half" elements is chosen as the minimum possible shift because
 // we must keep each row and column 256-bit aligned, as required by nvcuda::wmma::load_matrix_sync.
+// H100 32个bank，每个bank宽度为4 bytes
 #define SKEW_HALF 16
 
 #define checkKernelErrors(expr)                                                               \
@@ -175,6 +176,7 @@
 
 using namespace nvcuda;
 
+// 在 CPU 端初始化输入矩阵
 __host__ void init_host_matrices(half *a, half *b, float *c)
 {
     for (int i = 0; i < M_GLOBAL; i++) {
@@ -194,34 +196,53 @@ __host__ void init_host_matrices(half *a, half *b, float *c)
     }
 }
 
+// 核心高性能 Tensor Core kernel
+// D = alpha * A * B + beta * C
+// 使用 nvcuda::wmma API，也就是 Tensor Core 的 warp-level matrix multiply API
+// 一个 CTA/thread block 有 8 个 warp
+// 每个 CTA 处理一个大的 128 x 128 输出 tile
+// 每个 warp 负责多个 16 x 16 WMMA 小 tile
+// 把 A、B、C/D 的 tile 缓存在 shared memory 里
+// 用 SKEW_HALF 给 shared memory 做 padding，减少 bank conflict
 __global__ void compute_gemm(const half *A, const half *B, const float *C, float *D, float alpha, float beta)
 {
+    // 动态shared memory
+    // 每一行有 CHUNK_K * K + SKEW_HALF （4 * 16 + 16 = 80）个 half，行数由 SHMEM_SZ 决定
+    // CHUNK_K * K = 当前 K chunk 的真实数据
+    // SKEW_HALF   = padding，用来减少 shared memory bank conflict
+    // shared mem 有bank用于并行读
     extern __shared__ half shmem[][CHUNK_K * K + SKEW_HALF];
 
     // Warp and lane identification.
+    // 没有warpM和warpN，只有warpID。一个 warp 算 2 x 4 = 8 个 16x16 D tile。
     const unsigned int warpId = threadIdx.x / WARP_SIZE;
     const unsigned int laneId = threadIdx.x % WARP_SIZE;
 
     // Offset in shared memory from which the B matrix is stored.
+    // shmem_idx_b_off = 128，shmem 的前 128 行留给 A，从第 128 行开始放 B
     const size_t shmem_idx_b_off = BLOCK_COL_TILES * M;
 
     // This pointer is used to access the C and D matrix tiles this warp computes.
+    // 给当前 warp 做 WMMA load/store C/D fragment 用
     float *shmem_warp_tile_ptr =
         (float *)&shmem[0][0] + (warpId / 2) * SHMEM_STRIDE * K * 2 + (warpId % 2) * SHMEM_OFFSET;
 
     // This pointer is used to stream the C and D matrices block-wide tile to and
     // from shared memory.
+    // 给当前 warp 从 global memory 连续搬 C 到 shared，以及最后从 shared 连续搬 D 到 global 用
     float *shmem_warp_stream_ptr = (float *)&shmem[0][0] + warpId * SHMEM_STRIDE * K;
 
     // Adjust the beta scaler, as it'll be multiplied by alpha at the end of
     // each tile computation. Technically this is not generally correct (may
     // result in a loss of precision). Zero still needs to be specially handled
     // though.
+    // 目的：D = alpha * (A * B + beta_adjusted * C)
     beta /= alpha;
 
     // Each CTA slides along the 128 x 128 tiles from the top left corner of the
     // matrix to the right and down, and selects the next tile to compute. Once
     // there's no such tile, all warps in this CTA exit.
+    // 每个 block 怎么选择自己要算哪个 128x128 D tile。
     for (unsigned int block_pos = blockIdx.x;; block_pos += gridDim.x) {
         const unsigned int block_tile_i = ((block_pos * BLOCK_ROW_TILES) / N_TILES) * (BLOCK_COL_TILES);
         const unsigned int block_tile_j = (block_pos * BLOCK_ROW_TILES) % N_TILES;
@@ -357,7 +378,9 @@ __global__ void compute_gemm(const half *A, const half *B, const float *C, float
                     c[i][j].x[t] *= alpha;
 
                 float *tile_ptr = shmem_warp_tile_ptr + i * SHMEM_STRIDE * K + j * N;
-
+                // register -> shared mem
+                // wmma::store_matrix_sync 可以把 WMMA fragment 写到 shared memory，也可以直接写到 global memory。
+                // 关键不在函数名变不变，而在你传进去的指针指向哪里。
                 wmma::store_matrix_sync(tile_ptr, c[i][j], SHMEM_STRIDE, C_LAYOUT);
             }
         }
@@ -369,6 +392,11 @@ __global__ void compute_gemm(const half *A, const half *B, const float *C, float
         float *dst_gmem_warp_stream_ptr = &D[gmem_idx];
 
 #pragma unroll
+        // shared memory -> global mem
+        // 不直接 store_matrix_sync 到 global，
+        // 是因为先把每个 warp 的碎片结果整理进 shared memory，
+        // 再由整个 CTA 做连续、合并的 int4 写回，
+        // global memory 写入模式更规整。
         for (int i = 0; i < K; i++) {
             *((int4 *)(dst_gmem_warp_stream_ptr + GLOBAL_MEM_STRIDE * i) + laneId) =
                 *((int4 *)(shmem_warp_stream_ptr + SHMEM_STRIDE * i) + laneId);
@@ -386,40 +414,99 @@ __global__ void compute_gemm(const half *A, const half *B, const float *C, float
 // designed for
 //       demonstration purposes only to show the CUDA WMMA API use without
 //       relying on availability of the shared memory.
+// 简化版 WMMA kernel，主要用于教学和
+// 但相比 compute_gemm：
+// 1. 不做复杂的 shared memory 分块缓存
+// 2.每个 warp 基本负责一个 16 x 16 输出 tile
+// 3.直接从 global memory 读取 A、B、C
+// 4.用 wmma::load_matrix_sync
+// 5.用 wmma::mma_sync
+// 6.用 wmma::store_matrix_sync
+// 所以它更容易理解 WMMA API 的基本用法，但性能不如 compute_gemm
+// A 是 m_ld x k_ld
+// B 是 k_ld x n_ld
+// C/D 是 m_ld x n_ld
+// 这里用 half 做输入，是因为 Tensor Core 擅长处理半精度输入；累加和输出用 float，这样精度更好。
+// half  = 16-bit 浮点数，2 字节
+// float = 32-bit 浮点数，4 字节
+// 4096*4096
+/* 高性能的核心原因：
+1. compute_gemm 用 shared memory 做 block 内数据缓存，从而显著增加 A/B 数据复用，减少 global memory 重复读取
+        simple_wmma_gemm:
+            每个 warp 自己从 global memory 读 A/B
+            一个 warp 算一个 16x16 D tile
+            相邻 warp 之间复用关系没有显式利用
+        compute_gemm:
+            一个 block 算 128x128 D tile
+            block 先把当前 K chunk 的 A/B 搬到 shared memory
+            8 个 warp 共同从 shared memory 读取 A/B
+            同一份 A/B 被多个 warp、多个输出 tile 反复使用
+2. 此外还有几个辅助优化：
+        C/D 也通过 shared memory 整理读写
+        int4 向量化搬运
+        SKEW_HALF 减少 shared memory bank conflict
+        每个 warp 计算多个 16x16 tile，提高计算密度
+*/
 __global__ void
 simple_wmma_gemm(half *a, half *b, float *c, float *d, int m_ld, int n_ld, int k_ld, float alpha, float beta)
 {
     // Leading dimensions. Packed with no transpositions.
+    // 这里的 ld 是 leading dimension，可以先理解成“矩阵在内存里跨一行或一列要跳多少个元素”。
     int lda = k_ld;
     int ldb = k_ld;
     int ldc = n_ld;
 
     // Tile using a 2D grid
+    // 一个 warp 计算一个 16x16 的tile
+    // blockDim.x = 128，blockDim.y = 4;
+    // warpM: 第几个 16 行 tile
     int warpM = (blockIdx.x * blockDim.x + threadIdx.x) / warpSize;
+    // warpN: 第几个 16 列 tile，不用“/warpSize”，因为blockDim.x = 128，blockDim.y = 4，本身就是个4*4的warp矩阵
     int warpN = (blockIdx.y * blockDim.y + threadIdx.y);
 
     // Declare the fragments
+    // fragment 可以先理解成：Tensor Core / WMMA 使用的一小块矩阵数据，存在 warp 的寄存器状态里。
+    // 不是单个线程独占一个完整矩阵，而是 一个 warp 合作持有一个 fragment。每个线程持有 fragment 的一部分，CUDA 帮你安排具体分布。
+    // wmma::fragment<> 的第一个模板参数叫 Use，表示这个 fragment 在矩阵乘加里的“角色”，常见就三个
+    // a_frag: 这是乘法左边的矩阵 A，元素是 half，内存按 row-major 解释。
     wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a_frag;
+    // b_frag: 这是乘法右边的矩阵 B，元素是 half，内存按 col-major 解释。
     wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> b_frag;
+    // acc_frag：表示当前输出 D 的中间累加结果，类型是 float。
+    // 之后每走一个 K 方向 tile，就累加一次：acc_frag = acc_frag + a_frag * b_frag
     wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float>              acc_frag;
     wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float>              c_frag;
 
+    // 把累加器清零
     wmma::fill_fragment(acc_frag, 0.0f);
 
     // Loop over k
+    // 循环次数：4096 / 16 = 256 次
+    // acc += A[:, 0:16]    * B[0:16, :]
+    // acc += A[:, 16:32]   * B[16:32, :]
+    // acc += A[:, 32:48]   * B[32:48, :]
+    // ...
+    // acc += A[:, 4080:4096] * B[4080:4096, :]
     for (int i = 0; i < k_ld; i += WMMA_K) {
+        // 表示当前这次 K 循环，A tile 从哪一列开始
         int aCol = i;
+        // 表示当前 warp 要读的 A tile 从哪一行开始
         int aRow = warpM * WMMA_M;
+        // 表示当前 warp 要读的 B tile 从哪一列开始。
         int bCol = warpN * N;
+        // 表示当前这次 K 循环，B tile 从哪一行开始
         int bRow = i;
 
         // Bounds checking
         if (aRow < m_ld && aCol < k_ld && bRow < k_ld && bCol < n_ld) {
             // Load the inputs
+            // 没有把数据从 global memory 加载到 shared memory 的步骤。
+            // 直接global memory -> WMMA fragment/register
             wmma::load_matrix_sync(a_frag, a + aCol + aRow * lda, lda);
             wmma::load_matrix_sync(b_frag, b + bRow + bCol * ldb, ldb);
 
             // Perform the matrix multiplication
+            // acc_frag = a_frag * b_frag + acc_frag，累加结果
             wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
         }
     }
@@ -432,15 +519,18 @@ simple_wmma_gemm(half *a, half *b, float *c, float *d, int m_ld, int n_ld, int k
     if (cRow < m_ld && cCol < n_ld) {
         wmma::load_matrix_sync(c_frag, c + cCol + cRow * ldc, ldc, wmma::mem_row_major);
 
+        // 对 fragment 里的每个元素做：
         for (int i = 0; i < c_frag.num_elements; i++) {
             c_frag.x[i] = alpha * acc_frag.x[i] + beta * c_frag.x[i];
         }
 
         // Store the output
+        // register fragment -> global memory D，寄存器直接写回gloabl mem
         wmma::store_matrix_sync(d + cCol + cRow * ldc, c_frag, ldc, wmma::mem_row_major);
     }
 }
 
+// 这是 CPU 版本的矩阵乘法，用于校验 GPU 结果。
 __host__ void matMultiplyOnHost(half  *A,
                                 half  *B,
                                 float *C,
@@ -470,8 +560,10 @@ int main(int argc, char **argv)
 {
     printf("Initializing...\n");
 
+    // 选择 CUDA device
     int dev = findCudaDevice(argc, (const char **)argv);
 
+    // 获取 CUDA device 属性
     cudaDeviceProp deviceProp;
     checkCudaErrors(cudaGetDeviceProperties(&deviceProp, dev));
 
@@ -486,6 +578,7 @@ int main(int argc, char **argv)
     printf("N: %d (%d x %d)\n", N_GLOBAL, N, N_TILES);
     printf("K: %d (%d x %d)\n", K_GLOBAL, K, K_TILES);
 
+    // 声明 CPU 端矩阵
     half  *A_h = NULL;
     half  *B_h = NULL;
     float *C_h = NULL;
@@ -494,6 +587,7 @@ int main(int argc, char **argv)
     float *result_host = NULL;
 #endif
 
+    // 分配 CPU 端矩阵内存
     A_h = (half *)malloc(sizeof(half) * M_GLOBAL * K_GLOBAL);
     B_h = (half *)malloc(sizeof(half) * K_GLOBAL * N_GLOBAL);
     C_h = (float *)malloc(sizeof(float) * M_GLOBAL * N_GLOBAL);
@@ -502,11 +596,13 @@ int main(int argc, char **argv)
     result_host = (float *)malloc(sizeof(float) * M_GLOBAL * N_GLOBAL);
 #endif
 
+    // 声明 GPU 端矩阵
     half  *A = NULL;
     half  *B = NULL;
     float *C = NULL;
     float *D = NULL;
 
+    // 分配 GPU 端矩阵内存
     checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&A), sizeof(half) * M_GLOBAL * K_GLOBAL));
     checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&B), sizeof(half) * N_GLOBAL * K_GLOBAL));
     checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&C), sizeof(float) * M_GLOBAL * N_GLOBAL));
@@ -521,11 +617,13 @@ int main(int argc, char **argv)
 
     printf("Preparing data for GPU...\n");
 
+    // 复制数据从 CPU 到 GPU
     checkCudaErrors(cudaMemcpy(A, A_h, sizeof(half) * M_GLOBAL * K_GLOBAL, cudaMemcpyHostToDevice));
     checkCudaErrors(cudaMemcpy(B, B_h, sizeof(half) * N_GLOBAL * K_GLOBAL, cudaMemcpyHostToDevice));
     checkCudaErrors(cudaMemcpy(C, C_h, sizeof(float) * M_GLOBAL * N_GLOBAL, cudaMemcpyHostToDevice));
     checkCudaErrors(cudaMemset(D, 0, sizeof(float) * M_GLOBAL * N_GLOBAL));
 
+    // 计算所需的 shared memory 大小：max(A_tile + B_tile, C_or_D_tile)
     enum {
         // Compute the right amount of shared memory to request.
         // We need shared memory to hold per-CTA C and D matrix tiles, and to cache
@@ -544,11 +642,13 @@ int main(int argc, char **argv)
 
     cudaEvent_t start, stop;
 
+    // 创建 CUDA event 用于测量 kernel 执行时间
     checkCudaErrors(cudaEventCreate(&start));
     checkCudaErrors(cudaEventCreate(&stop));
     checkCudaErrors(cudaEventRecord(start));
 
     // If enough shared memory available on the GPU use high performant kernel
+    // 如果 GPU 的 shared memory 足够，使用高性能 kernel compute_gemm
     if (deviceProp.sharedMemPerMultiprocessor >= SHMEM_SZ) {
         printf("Computing... using high performance kernel compute_gemm \n");
 
@@ -578,7 +678,9 @@ int main(int argc, char **argv)
 #endif
     }
 
+    // 记录结束时间
     checkCudaErrors(cudaEventRecord(stop));
+    // 等待事件完成
     checkCudaErrors(cudaEventSynchronize(stop));
 
 #if CPU_DEBUG
@@ -601,6 +703,7 @@ int main(int argc, char **argv)
     checkCudaErrors(cudaEventElapsedTime(&milliseconds, start, stop));
 
     printf("Time: %f ms\n", milliseconds);
+    // 计算 TFLOPS
     printf("TFLOPS: %.2f\n",
            static_cast<double>((static_cast<double>(M_GLOBAL) * N_GLOBAL * K_GLOBAL * 2) / (milliseconds / 1000.))
                / 1e12);
